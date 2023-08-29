8c5f223e4e42d856b502476894eea3d2f6fe79ab
oluobamzy <oluobamzy@gmail.com>
2023-08-29 14:40:52 -0400

    add feature-cypress tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,15 @@
+
+context('Home', () => {
+  it('should load the home page', () => {
+    cy.visit('http://localhost:3000')
+  
+})
+
+it("There is products on the page", () => {
+  cy.get(".products article").should("be.visible");
+});
+
+it("There is 12 products on the page", () => {
+  cy.get(".products article").should("have.length", 12);
+});
+})
